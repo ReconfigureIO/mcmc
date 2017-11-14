f12b69cdbace467a5424c01716fcb5fb99cf5443
Vanessa McHale <vanessa.mchale@reconfigure.io>
2017-11-14 17:18:41 -0600

    more sophistication

--- main.go
+++ main.go
@@ -17,19 +17,18 @@ func VectorSum(x [4]uint32) uint32 {
 	return sum
 }
 
-// TODO check it sums to 1.
-func MatrixVector(x [16]uint32, a [4]uint32) [4]uint32 {
+func MatrixVector(x [4][4]uint32, a [4]uint32) [4]uint32 {
 	b := [4]uint32{}
 	for i := 0; i <= 3; i++ {
 		for j := 0; j <= 3; j++ {
-			b[i] = b[i] + a[i]*x[4*i+j] //[j]
+			b[i] = b[i] + a[i]*x[i][j]
 		}
 	}
 	return b
 }
 
-func MatrixIterate(n int, x [16]uint32, a [4]uint32) [4]uint32 {
-	b := a //[4]uint32{}
+func MatrixIterate(n int, x [4][4]uint32, a [4]uint32) [4]uint32 {
+	b := a
 	for i := 0; i < n; i++ {
 		b = MatrixVector(x, b)
 	}
@@ -62,10 +61,19 @@ func Top(
 	// Since we're not reading anything from memory, disable those reads
 	go axiprotocol.ReadDisable(memReadAddr, memReadData)
 
-	m := [16]uint32{}
+	m := [4][4]uint32{}
+	m[0][0] = 1
+	m[1][1] = 1
+	m[2][2] = 1
+	m[3][3] = 1
 	v := [4]uint32{}
+	v[0] = 1
+	v[1] = 4
+	v[2] = 4
+	v[3] = 1
 
 	iter := a >> 30
+
 	// matrix iterate can't read from memory?
 	x := MatrixIterate(4, m, v)
 
@@ -74,7 +82,7 @@ func Top(
 	//msg := <-outputChannel
 
 	// Calculate the value
-	val := a + b + VectorSum(x)
+	val := VectorSum(x)
 
 	// Write it back to the pointer the host requests
 	aximemory.WriteUInt32(
