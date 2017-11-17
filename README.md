357d9c6cb798baf703c2af4d0b379068a6d4372f
Vanessa McHale <vanessa.mchale@reconfigure.io>
2017-11-17 07:51:57 -0600

    benchmarks working

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 An example of Markov Chain Monte Carlo on an FPGA. Uses the [Reconfigure IO](http://docs.reconfigure.io/features.html) compiler.
 
 Currently a WIP.
+
+<!-- 0.007774 ms -->
---
+++ examples/cmd/bench-mcmc/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	//	"encoding/binary"
+	// "math/rand"
+	"testing"
+	"xcl"
+
+	"ReconfigureIO/reco-sdaccel/benchmarks"
+)
+
+const (
+	MAX_BIT_WIDTH       = 16
+	HISTOGRAM_BIT_WIDTH = 9
+	HISTOGRAM_WIDTH     = 1 << 9
+)
+
+func main() {
+	Process("Markov Chain Monte Carlo")
+}
+
+func Process(name string) {
+	world := xcl.NewWorld()
+	defer world.Release()
+
+	program := world.Import("kernel_test")
+	defer program.Release()
+
+	krnl := program.GetKernel("reconfigure_io_sdaccel_builder_stub_0_1")
+	defer krnl.Release()
+
+	f := func(B *testing.B) {
+		doit(world, krnl, B)
+	}
+
+	bm := testing.Benchmark(f)
+	benchmarks.GipedaResults(name, bm)
+}
+
+func doit(world xcl.World, krnl *xcl.Kernel, B *testing.B) {
+	B.SetBytes(4)
+	B.ReportAllocs()
+
+	// The data we'll send to the kernel for processing
+	//seed := rand.Uint32()
+
+	// don't care about these
+	buff := world.Malloc(xcl.WriteOnly, 4)
+	defer buff.Free()
+
+	krnl.SetArg(0, 100)
+	krnl.SetArg(1, 100)
+	krnl.SetMemoryArg(2, buff)
+
+	krnl.Run(1, 1, 1)
+	B.StopTimer()
+}
