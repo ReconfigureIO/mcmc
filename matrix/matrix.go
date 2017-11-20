6551953684facb692a80d20d65b16985a58f4bad
Vanessa McHale <vanessa.mchale@reconfigure.io>
2017-11-20 12:23:17 -0600

    fix speed of benchmark

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -9,26 +9,21 @@ func VectorSum(x [8]uint32) uint32 {
 	return sum
 }
 
-func matrixVector(c <-chan uint32, a [8]uint32) [8]uint32 {
+func MatrixIterate(n int, c <-chan uint32, a [8]uint32) [8]uint32 {
 	x := [64]uint32{}
 	go func() {
 		for i := 0; i < 64; i++ {
 			x[i] = <-c
 		}
 	}()
-	b := [8]uint32{}
-	for i := 0; i <= 7; i++ {
-		for j := 0; j <= 7; j++ {
-			b[i] = b[i] + a[i]*x[8*i+j]
-		}
-	}
-	return b
-}
-
-func MatrixIterate(n int, c <-chan uint32, a [8]uint32) [8]uint32 {
 	b := a
 	for i := 0; i < n; i++ {
-		b = matrixVector(c, b)
+		b = [8]uint32{}
+		for i := 0; i <= 7; i++ {
+			for j := 0; j <= 7; j++ {
+				b[i] = b[i] + a[i]*x[8*i+j]
+			}
+		}
 	}
 	return b
 }
